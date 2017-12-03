d302d3d0df7a5a6c5264175d62cd505161996181
ajpetrosky <petroskyandrew@gmail.com>
2017-12-03 01:21:36 -0500

    Partial implementation of Tests.hs

--- Tests.hs
+++ Tests.hs
@@ -6,14 +6,16 @@ module Tests where
 
 import Test.HUnit
 import Test.QuickCheck
+import Compress
+import Decompress
+import Control.Monad (liftM2)
+import qualified Data.ByteString.Lazy as B
 
 {-
 Questions:
-1. How do we handle endianess?
-2. What are more quickCheck properties we may be missing to test?
-Given internal table and next string to encode, should be determ. (test add encoding)
-3. Are there other tests in general we should include?
-Unit tests of Puts and Gets monads for our own understanding
+1. How to remove file extension?
+2. Help with test prop_loseless and backslashes
+3. How to optimize compress (ex. hard.txt)
 -}
 
 -- Unit tests for compressing small strings
@@ -21,10 +23,25 @@ Unit tests of Puts and Gets monads for our own understanding
 -- Unit tests for decompressing small bytestrings
 
 -- Test that (compression -> decompression) is lossless
-prop_loseless :: String -> Bool
+prop_loseless :: String -> Property
+prop_loseless s = '\\' `notElem` s ==> s == (decompress . compress) s
 
 -- Test with small alphabet and many repeats, compressed file is smaller
--- Own gen for String (put string into new type with new arb)
-prop_ratio :: String -> Bool
+prop_ratio :: SmallAlphabet -> Property
+prop_ratio s = length (toString s) > 200 ==> lenBs <= lenS' where
+  s' = toString s
+  bs = compress s'
+  lenS' = length s'
+  lenBs = length (B.unpack bs)
+
+newtype SmallAlphabet = SmallAlphabet String deriving (Eq, Ord, Show, Read)
+
+instance Arbitrary SmallAlphabet where
+  arbitrary  = SmallAlphabet <$> sized gen where
+    gen n = frequency [(1, return []), (1000, liftM2 (++) (elements ["a","ab","aa","b","bb","ba"]) (gen 1000))]
+
+toString :: SmallAlphabet -> String
+toString (SmallAlphabet s) = s
 
 -- Informal testing of command line tool
+-- *Complete*
